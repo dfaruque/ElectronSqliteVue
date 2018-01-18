ebb981ba9b166e77e5aff1410b415b5a146c2b80
dfaruque <dfaruque@gmail.com>
2018-01-18 18:22:39 +0600

    added package sqlite3 and rebuild-sqlite3-win32 script

--- sample-crud-app/main.js
+++ sample-crud-app/main.js
@@ -16,11 +16,12 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+ // mainWindow.loadURL(url.format({
+ //   pathname: path.join(__dirname, 'index.html'),
+ //   protocol: 'file:',
+ //   slashes: true
+ // }))
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
    mainWindow.webContents.openDevTools()
